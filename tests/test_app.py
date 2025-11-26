03b4a15fd26c18be209d2908ec9d5244b9929fd0
Hugo Mello <hugo.mello8@gmail.com>
2025-11-25 21:18:39 -0300

    Criando repositório

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_api.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    """
+    Teste com 3 etapas (AAA):
+    - Arrange (arranjo)
+    - Act (executar a coisa / agir)
+    - Assert (garantir que a coisa é a coisa e não diferente)
+    """
+
+    client = TestClient(app)  # Arrange
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
